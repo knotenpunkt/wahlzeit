8836e9c9f085329c3b2ee8761adf579b27050af8
Robin <Robin-Kreuzer@gmx.de>
2015-11-01 23:11:16 +0100

    final 2 commit fuer Hausaufgabe 4

--- src/main/java/org/wahlzeit/model/PhotoHausFactory.java
+++ src/main/java/org/wahlzeit/model/PhotoHausFactory.java
@@ -3,4 +3,13 @@ package org.wahlzeit.model;
 public class PhotoHausFactory extends PhotoFactory 
 {
 
+    /**
+     * Eigentlich waere es schoener sowas zu machen, also eine konkrete Klasse abgeleitet von der abstrakten Factory
+     * Ist aber nicht explizit erwuenscht, von daher hab ich jetzt doch die einfachere Variante gewaehlt,
+     * zumal das persistieren ziemlicht suckt und mit jedem kleinen Zusatzfeature das man einbaut bricht^^
+     * 
+     * Weitere Infos hier: https://fsi.cs.fau.de/forum/thread/13063-Fehler-im-Design
+     */
+    
+    
 }
