38f4c645b822e275b5a41ebb684880beb1673547
knotenpunkt <robin-unbekannt@web.de>
2015-12-16 18:53:46 +0100

    type-object-pattern implementierung2 noch ein paar comments eingefuegt

--- src/main/java/org/wahlzeit/model/HausType.java
+++ src/main/java/org/wahlzeit/model/HausType.java
@@ -8,9 +8,9 @@ public class HausType {
 
 	public HausType(boolean luxusSegment, long preisklasse,
 			String luxuxBezeichnung) {
-		this.luxusSegment = luxusSegment;
-		this.preisklasse = preisklasse;
-		this.luxuxBezeichnung = luxuxBezeichnung;
+		this.luxusSegment = luxusSegment; //wird das ganze schon als luxus gewertet, oder eben nicht
+		this.preisklasse = preisklasse; //preisklasse 50 000 bedeutet bspw. ne Preisrange von 40-60K USD 
+		this.luxuxBezeichnung = luxuxBezeichnung; //bspw. Villa
 	}
 
 	public Haus createInstance() {
