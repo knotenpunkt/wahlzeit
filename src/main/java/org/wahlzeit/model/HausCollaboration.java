626198b05bd5c92f871cae3b5c4c5e5f0a051dee
knotenpunkt <robin-unbekannt@web.de>
2016-01-19 15:42:12 +0100

    Erste Gehversuche mit Hausaufgabe 14

---
+++ src/main/java/org/wahlzeit/model/HausCollaboration.java
@@ -0,0 +1,39 @@
+package org.wahlzeit.model;
+
+class HausCollobaration{}
+
+
+//Die Loesung von der aktuellen und letzten Hausaufgabe steht unten im Kommentar
+//Falls du vor Abgabe mein github-Repository besuchen solltest, dann empfehle ich dir dich nicht auf unteres zu verlassen
+//weil ich kA hab ob des stimmmt und es sich noch in den Anfaengen befindet^^
+//Wenn du der Meinung bist des ist komplett falsch, dann darfst du mich auch gerne über mein Github-Repository/Email/etc anschreiben
+
+
+
+/**
+	public collaboration ClientService //PhotoHausHaus
+	{
+		public role Client //PhotoHaus
+		{
+			public void setService(Service);
+			public Service getService();
+			
+		}
+		
+		
+		public role Service //Haus
+		{
+			//hat keine Referenz auf Haus
+		}
+		
+	}
+	
+
+public class PhotoHaus binds ClientService.Client()
+{
+	public Haus getHaus() binds getService();
+	public void setHaus(Haus) binds setService(Service);
+
+}
+
+**/
